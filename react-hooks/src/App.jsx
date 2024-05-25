d9d5e2ce235522110dec6c5fa18d833781d2f0b0
John-Sellers <sellers.e.john@gmail.com>
2024-05-25 16:05:16 -0400

    Boggled my mind with the useContext hook

--- react-hooks/src/App.jsx
+++ react-hooks/src/App.jsx
@@ -1,11 +1,19 @@
 import MyComponent from "./MyComponent"
 import Counter from "./Counter/Counter"
 import UseEffectPrac from "./UseEffect"
+import ComponentA from "./ComponentA"
 
 function App() {
-  return (
-    // <Counter />
-    <UseEffectPrac />
+  return (<>
+    {/* //useState Practice */}
+    {/* // <Counter /> */}
+
+    {/* // useEffect Practice */}
+    {/* <UseEffectPrac /> */}
+
+    {/* useContext Practice */}
+    <ComponentA />
+  </>
   )
 }
 
